ac8cad37350761ea71e59bc25c43644f6d9c5eb3
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-08-22 07:57:57 -0400

    set up links, set up pseudocode outline for js, set up html components

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//Create an array of words
+//Choose the current word randomly
+//Make underscores matching word length
+//Get user's guess (letter)
+//Check if guessed letter is right
+//If right, push to right array
+//If wrong, push ro wrong array
