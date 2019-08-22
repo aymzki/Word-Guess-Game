2f66651879321e97cc717afb6566f862d4941363
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-08-22 09:22:07 -0400

    first two lines js-- create array and choose current word randomly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,12 @@
 //Create an array of words
+var dogs = ["chihuahua", "beagle", "yorkie", "husky", "poodle", "corgi", "whippet", "collie"]
 //Choose the current word randomly
+var randomNum = Math.floor(Math.random()*dogs.length);
+let currentWord = dogs[randomNum];
+console.log(currentWord);
 //Make underscores matching word length
+
 //Get user's guess (letter)
 //Check if guessed letter is right
 //If right, push to right array
-//If wrong, push ro wrong array
+//If wrong, push to wrong array
