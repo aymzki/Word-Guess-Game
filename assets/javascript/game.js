7374575228e115517a1a41044329fe7e77d1fe53
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-08-23 01:02:30 -0400

    same number placeholders as length of word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,22 @@
 //Create an array of words
 var dogs = ["chihuahua", "beagle", "yorkie", "husky", "poodle", "corgi", "whippet", "collie"]
 //Choose the current word randomly
-var randomNum = Math.floor(Math.random()*dogs.length);
-let currentWord = dogs[randomNum];
+var randomNum = Math.floor(Math.random() * dogs.length);
+var currentWord = dogs[randomNum];
 console.log(currentWord);
-//Make underscores matching word length
+
+//Make underscores matching the word length
+var placeHolder = []
+for (var i = 0; i < currentWord.length; i++) {
+    console.log("-");
+    placeHolder.push("-");
+}
+console.log(placeHolder);
+
+//Put placeHolder in website as text
+var dashDiv = document.getElementById("placeholders");
+dashDiv.textContent = placeHolder.join(" ");
+
 
 //Get user's guess (letter)
 //Check if guessed letter is right
